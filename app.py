a34cc1c7ac2cc81c6fb54efb172465c5368fff58
chittranjanpr <chittranjanjpr@gmail.com>
2023-04-15 01:22:31 -0400

    built the order page with the product listing

--- app.py
+++ app.py
@@ -14,8 +14,25 @@ def order_page():
     response = requests.get('http://127.0.0.1:8000/get_store_details')
     data = response.json()
     print(data)
-
-    return render_template('/mainPage/index.html', title = 'Home Page', appName = appName, data = data)
+    items = [
+    {
+      "name": "Chicken Pizza",
+      "price": 10
+    },
+    {
+      "name": "Cheese Pizza",
+      "price": 15
+    },
+    {
+      "name": "Chicken Burger",
+      "price": 25
+    },
+    {
+      "name": "Cheese Burger",
+      "price": 20
+    }
+  ]
+    return render_template('/mainPage/index.html', title = 'Home Page', appName = appName, data = data, menu_items = items)
 
 if __name__ == '__main__':
     app.run(debug=True)
